759facdcbd2734e93b9d64326cfee3288cc0c3b0
tahaamin2013 <tahaamindob2013@gmail.com>
2024-05-28 01:42:49 +0500

    add uniqe words finder

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -10,18 +10,45 @@ import Link from "next/link";
 const Tools = () => {
   return (
     <div className="flex flex-wrap px-6 justify-center mt-6 w-full">
-      <h1 className="text-4xl font-bold">⚒ Devkins Tools</h1>
-      <div className="flex flex-wrap px-6 justify-center mt-6 gap-[50px] w-full">
+      <h1 className="text-4xl font-bold">🛠 Devkins Tools</h1>
+      <div className="flex flex-wrap px-6 justify-center mt-6 gap-y-[30px] gap-[50px] w-full">
         <Accordion
           type="single"
           collapsible
           defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
           className="bg-white rounded-xl p-3 pt-0 border h-fit"
         >
-          <AccordionItem value="item-1" className="w-[320px]">
+          <AccordionItem value="item-1" className="w-[350px]">
             <AccordionTrigger>
               SEO (Search Engine Optimization) Tools
             </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-3">
+              <Link
+                href="/uniqe-words-finder"
+                className="border-b w-fit border-black"
+              >
+                Unique Words Finder in Related Keywords
+              </Link>
+              <Link href="/fsd" className="border-b w-fit border-black">
+                Tool Title
+              </Link>
+              <Link href="/fsd" className="border-b w-fit border-black">
+                Tool Title
+              </Link>
+              <Link href="/fsd" className="border-b w-fit border-black">
+                Tool Title
+              </Link>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion
+          type="single"
+          collapsible
+          defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
+          className="bg-white rounded-xl p-3 pt-0 border h-fit"
+        >
+          <AccordionItem value="item-1" className="w-[350px]">
+            <AccordionTrigger>Simple Tools</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-3">
               <Link href="/fsd" className="border-b w-fit border-black">
                 Tool Title
@@ -44,10 +71,8 @@ const Tools = () => {
           defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
           className="bg-white rounded-xl p-3 pt-0 border h-fit"
         >
-          <AccordionItem value="item-1" className="w-[320px]">
-            <AccordionTrigger>
-              SEO (Search Engine Optimization) Tools
-            </AccordionTrigger>
+          <AccordionItem value="item-1" className="w-[350px]">
+            <AccordionTrigger>Modern Tools</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-3">
               <Link href="/fsd" className="border-b w-fit border-black">
                 Tool Title
@@ -70,10 +95,8 @@ const Tools = () => {
           defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
           className="bg-white rounded-xl p-3 pt-0 border h-fit"
         >
-          <AccordionItem value="item-1" className="w-[320px]">
-            <AccordionTrigger>
-              SEO (Search Engine Optimization) Tools
-            </AccordionTrigger>
+          <AccordionItem value="item-1" className="w-[350px]">
+            <AccordionTrigger>Mathametics Tools</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-3">
               <Link href="/fsd" className="border-b w-fit border-black">
                 Tool Title
@@ -96,10 +119,8 @@ const Tools = () => {
           defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
           className="bg-white rounded-xl p-3 pt-0 border h-fit"
         >
-          <AccordionItem value="item-1" className="w-[320px]">
-            <AccordionTrigger>
-              SEO (Search Engine Optimization) Tools
-            </AccordionTrigger>
+          <AccordionItem value="item-1" className="w-[350px]">
+            <AccordionTrigger>English Tools</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-3">
               <Link href="/fsd" className="border-b w-fit border-black">
                 Tool Title
@@ -122,36 +143,8 @@ const Tools = () => {
           defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
           className="bg-white rounded-xl p-3 pt-0 border h-fit"
         >
-          <AccordionItem value="item-1" className="w-[320px]">
-            <AccordionTrigger>
-              SEO (Search Engine Optimization) Tools
-            </AccordionTrigger>
-            <AccordionContent className="flex flex-col gap-3">
-              <Link href="/fsd" className="border-b w-fit border-black">
-                Tool Title
-              </Link>
-              <Link href="/fsd" className="border-b w-fit border-black">
-                Tool Title
-              </Link>
-              <Link href="/fsd" className="border-b w-fit border-black">
-                Tool Title
-              </Link>
-              <Link href="/fsd" className="border-b w-fit border-black">
-                Tool Title
-              </Link>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion
-          type="single"
-          collapsible
-          defaultValue="item-1" // Set defaultValue to "item-1" to open the accordion by default
-          className="bg-white rounded-xl p-3 pt-0 border h-fit"
-        >
-          <AccordionItem value="item-1" className="w-[320px]">
-            <AccordionTrigger>
-              SEO (Search Engine Optimization) Tools
-            </AccordionTrigger>
+          <AccordionItem value="item-1" className="w-[350px]">
+            <AccordionTrigger>Example Tools</AccordionTrigger>
             <AccordionContent className="flex flex-col gap-3">
               <Link href="/fsd" className="border-b w-fit border-black">
                 Tool Title
---
+++ src/app/uniqe-words-finder/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Copy } from "lucide-react";
+import { useState } from "react";
+import { Check } from "lucide-react";
+
+export default function KeywordMatcher() {
+  const [mainKeyword, setMainKeyword] = useState("");
+  const [relatedKeywords, setRelatedKeywords] = useState("");
+  const [nonMatchedKeywords, setNonMatchedKeywords] = useState([]);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const processKeywords = () => {
+    const mainKeywordLowerCase = mainKeyword.toLowerCase();
+    const relatedKeywordsArr = relatedKeywords
+      .toLowerCase()
+      .split("\n")
+      .flatMap((keyword) => keyword.split(" "))
+      .filter(Boolean);
+
+    const nonMatched = relatedKeywordsArr.filter(
+      (word) => !mainKeywordLowerCase.includes(word)
+    );
+
+    const nonMatchedLines: any = relatedKeywords
+      .split("\n")
+      .map((line) =>
+        line
+          .split(" ")
+          .filter((word) => nonMatched.includes(word))
+          .join(" ")
+      )
+      .filter(Boolean);
+    // @ts-ignore
+    const uniqueNonMatched = [...new Set(nonMatched)];
+
+    setNonMatchedKeywords(nonMatchedLines);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(nonMatchedKeywords.join("\n"))
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000); // Revert to copy icon after 2 seconds
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
+  };
+
+  return (
+    <div className="flex h-screen px-5   justify-center items-center">
+      <div className="bg-white border shadow-2xl rounded-lg p-8 w-full max-w-3xl">
+        <h1 className="text-3xl font-bold mb-6 text-center text-black   ">
+          Unique Words Finder in Related Keywords
+        </h1>
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="flex-1">
+            <Input
+              type="text"
+              value={mainKeyword}
+              onChange={(e: any) => setMainKeyword(e.target.value)}
+              placeholder="Main Keyword:"
+              className="mb-4 text-gray-800"
+            />
+            <Textarea
+              placeholder="Related Keywords"
+              value={relatedKeywords}
+              onChange={(e: any) => setRelatedKeywords(e.target.value)}
+              className="mb-4 resize-y min-h-[70px] text-gray-800"
+            />
+            <Button
+              size="lg"
+              className="bg-primary text-white font-bold py-2 px-4 w-full mb-4"
+              onClick={processKeywords}
+            >
+              Find Different
+            </Button>
+          </div>
+
+          <div className="flex gap-3">
+            <div>
+              <h2 className="text-2xl mb-2 text-black">Unique Keywords:</h2>
+              <ul className="list-disc pl-6 text-black">
+                {nonMatchedKeywords.map((word, index) => (
+                  <li key={index} className="mb-2">
+                    {word}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Button onClick={copyToClipboard}>
+              {isCopied ? (
+                <span className="text-green-600">
+                  <Check />
+                </span>
+              ) : (
+                <Copy className="text-white" />
+              )}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
